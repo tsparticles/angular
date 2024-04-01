76a4ce4d2b4e978d4f2ed97719595e6ca4b661a6
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2024-04-01 19:52:25 +0200

    build: updated deps

--- apps/angular-demo/src/app/app.component.ts
+++ apps/angular-demo/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from "@angular/core";
 import type { Container, Engine, ISourceOptions } from "@tsparticles/engine";
-import { loadFull } from "tsparticles";
 import configs from "@tsparticles/configs";
 import { NgParticlesService } from "@tsparticles/angular";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: [ "./app.component.css" ]
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title = "angular";
@@ -49,6 +48,8 @@ export class AppComponent {
   ngOnInit(): void {
     void this.ngParticlesService.init(async (engine: Engine) => {
       console.log("init", engine);
+
+      const { loadFull } = await import("tsparticles");
 
       await loadFull(engine);
     });
--- components/particles/projects/ng-particles/src/lib/ng-particles.component.ts
+++ components/particles/projects/ng-particles/src/lib/ng-particles.component.ts
@@ -1,13 +1,13 @@
 import {
-  AfterViewInit,
-  Component,
-  EventEmitter,
-  Inject,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output,
-  PLATFORM_ID,
+    AfterViewInit,
+    Component,
+    EventEmitter,
+    Inject,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+    PLATFORM_ID,
 } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 import { tsParticles } from '@tsparticles/engine';
@@ -16,49 +16,53 @@ import { IParticlesProps } from './ng-particles.module';
 import { NgParticlesService } from './ng-particles.service';
 
 @Component({
-  selector: 'ngx-particles',
-  template: '<div [id]="id"></div>',
+    selector: 'ngx-particles',
+    template: '<div [id]="id"></div>',
 })
 export class NgxParticlesComponent implements OnInit, AfterViewInit, OnDestroy {
-  @Input() options?: IParticlesProps;
-  @Input() url?: string;
-  @Input() id: string;
-  @Output() particlesLoaded: EventEmitter<Container> = new EventEmitter<Container>();
+    @Input() options?: IParticlesProps;
+    @Input() url?: string;
+    @Input() id: string;
+    @Output() particlesLoaded: EventEmitter<Container> = new EventEmitter<Container>();
 
-  private container?: Container;
+    private container?: Container;
 
-  constructor(
-    @Inject(PLATFORM_ID) protected platformId: string,
-    private readonly particlesService: NgParticlesService,
-  ) {
-    this.id = 'tsparticles';
-  }
-
-  public async ngOnInit() {
-    if (await this.particlesService.getInstallationStatus()) {
-      this.container?.destroy();
-      this.loadParticles();
-    }
-  }
-
-  public ngAfterViewInit(): void {
-    if (isPlatformServer(this.platformId)) {
-      return;
+    constructor(
+        @Inject(PLATFORM_ID) protected platformId: string,
+        private readonly particlesService: NgParticlesService,
+    ) {
+        this.id = 'tsparticles';
     }
 
-    this.loadParticles();
-  }
+    public async ngOnInit() {
+        this.particlesService.getInstallationStatus().subscribe(status => {
+            if (status) {
+                this.loadParticles();
+            }
 
-  public ngOnDestroy(): void {
-    this.container?.destroy();
-  }
+            this.container?.destroy();
+        });
+    }
 
-  private loadParticles(): void {
-    tsParticles.load({ id: this.id, url: this.url, options: this.options })
-      .then(container => {
-        this.container = container;
-        this.particlesLoaded.emit(container);
-      })
-      .catch(error => console.error(error));
-  }
+    public ngAfterViewInit(): void {
+        if (isPlatformServer(this.platformId)) {
+            return;
+        }
+
+        this.loadParticles();
+    }
+
+    public ngOnDestroy(): void {
+        this.container?.destroy();
+    }
+
+    private loadParticles(): void {
+        tsParticles
+            .load({ id: this.id, url: this.url, options: this.options })
+            .then(container => {
+                this.container = container;
+                this.particlesLoaded.emit(container);
+            })
+            .catch(error => console.error(error));
+    }
 }
